78f6d573faa26a497eabdbb297a2ff9cdbad01ab
Arash-SJPI22 <arash.eblaghian@lerniastudent.se>
2023-11-06 13:18:07 +0100

    Initial commit

---
+++ src/main/java/lernia/backosys/laboration02/Laboration02Application.java
@@ -0,0 +1,13 @@
+package lernia.backosys.laboration02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Laboration02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Laboration02Application.class, args);
+    }
+
+}
